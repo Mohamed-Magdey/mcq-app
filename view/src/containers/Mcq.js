eb7e8bfc7d20fcf66bbc49af46587ad9e900e9cf
mohamed <ragabmohame6@gmail.com>
2021-02-27 17:27:38 +0200

    Reafactor MCQ using react hooks

--- view/src/containers/Mcq.js
+++ view/src/containers/Mcq.js
@@ -1,62 +1,60 @@
-import React from 'react';
-import {connect} from 'react-redux';
+import React, {useState, useEffect} from 'react';
+import {useDispatch} from 'react-redux';
 import fetchQuestions from '../store/actions/questions';
 import { Button, Form, Grid, Header, Segment } from 'semantic-ui-react';
 import {newUser} from "../store/actions/user";
 import Test from './Test';
 import './Mcq.css';
 
-class Mcq extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-        name: '',
-        isSubmitted: false
-    }
-  }
+const Mcq = () => {
+    const [name, setName] = useState('');
+    const [isSubmitted, setIsSubmitted] = useState(false);
 
-  componentDidMount() {
-      this.props.fetchQuestions();
-  }
+    const dispatch = useDispatch();
 
-    handleInput = (e) => {
-        this.setState({name: e.target.value})
+    useEffect(() => {
+        dispatch(fetchQuestions());
+    }, []);
+
+
+    const handleInput = (e) => {
+        setName(e.target.value)
     };
 
-    handleSubmit = async (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
-        if(this.state.name) {
-            this.props.newUser(this.state.name);
-            await this.setState({isSubmitted: !this.state.isSubmitted, name: ""});
+        if(name) {
+            dispatch(newUser(name));
+            setIsSubmitted(!isSubmitted);
+            setName('')
         }
     };
 
-  render() {
-      if(this.state.isSubmitted) {
-          return <Test/>
-      }
-      return (
-          <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-              <Grid.Column style={{ maxWidth: 450 }}>
-                  <Header as='h1' content='MCQ APP' color='teal' textAlign='center'/>
-                  <Form size='large'>
-                      <Segment raised>
-                          <Form.Input fluid icon='user' iconPosition='left'
-                                      placeholder='Enter Your Name'
-                                      onChange={this.handleInput}
-                          />
-                          <Button color='teal'
-                                  fluid size='large'
-                                  onClick={this.handleSubmit}
-                                  content='Submit'
-                          />
-                      </Segment>
-                  </Form>
-              </Grid.Column>
-          </Grid>
-      )
-  }
-}
+    if(isSubmitted) {
+        return <Test/>
+    }
+
+    return (
+        <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+            <Grid.Column style={{ maxWidth: 450 }}>
+                <Header as='h1' content='MCQ APP' color='teal' textAlign='center'/>
+                <Form size='large'>
+                    <Segment raised>
+                        <Form.Input fluid icon='user' iconPosition='left'
+                                    placeholder='Enter Your Name'
+                                    onChange={handleInput}
+                        />
+                        <Button color='teal'
+                                fluid size='large'
+                                onClick={handleSubmit}
+                                content='Submit'
+                        />
+                    </Segment>
+                </Form>
+            </Grid.Column>
+        </Grid>
+    )
+};
 
 
-export default connect(null ,{fetchQuestions, newUser})(Mcq);
+export default Mcq;
